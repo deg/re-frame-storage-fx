a00606f99bb4542b68269e223603b3cfb4dc744e
David Goldfarb <deg@degel.com>
2018-06-14 11:10:32 +0300

    v0.1.1 - Update dependencies

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.degel/re-frame-storage-fx "0.1.0"
+(defproject com.degel/re-frame-storage-fx "0.1.1"
   :description "A re-frame effects handler to manage web storage"
   :url "https://github.com/deg/re-frame-storage-fx"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.542"]
-                 [re-frame "0.9.3"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [re-frame "0.10.5"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :plugins [[lein-npm "0.6.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
